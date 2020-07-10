e79ec0e581cf3a8c15f0ba75c01d3dba370de9f8
Mike Pilkington <mpilking@gmail.com>
2020-07-09 20:23:15 -0500

    Update for vss_carver.py

--- create_for508_mounts.py
+++ create_for508_mounts.py
@@ -2,15 +2,15 @@
 
 # create_for508_mounts.py will generate custom mount point directories for exercises in the SANS FOR508 class.  Run "python create_for508_mounts.py -h" for usage details.
 # by Mike Pilkington
-# version 1.0
+# version 1.1
 
 import os
 import argparse
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument('dir_suffix', help='Provide a descriptive suffix to append to new mount point directories.  For example, "nfury" will create /mnt/ewf_nfury, /mnt/windows_nfury, /mnt/vss_nfury, /mnt/shadow_nfury, and /mnt/shadow_nfury/vss* directories. This argument is required.')
-parser.add_argument('-n','--vss-number', type=int, nargs="?", dest="number", default=40, help='Optionally provide the number of vss subdirectories to create. The default is 40.')
+parser.add_argument('dir_suffix', help='Provide a descriptive suffix to append to new mount point directories.  For example, "nfury" will create /mnt/ewf_nfury, /mnt/windows_nfury, /mnt/vss_nfury, /mnt/vsscarve_nfury, /mnt/shadow_nfury, /mnt/shadow_nfury/vss*, /mnt/shadowcarve_nfury, and /mnt/shadowcarve_nfury/vss* directories. This argument is required.')
+parser.add_argument('-n','--vss-number', type=int, nargs="?", dest="number", default=50, help='Optionally provide the number of vss subdirectories to create. The default is 50.')
 parser.add_argument('-b','--base-path', nargs="?", dest="base", default='/mnt', help='Optionally provide the base path to create the mount directories. The default is "/mnt" (and must be run as root).')
 
 
@@ -31,12 +31,16 @@ def createdir(directory):
             os.makedirs(directory)
     except OSError:
         print ('Error: Creating directory. ' +  directory + ' (Must run as root for default base-path of /mnt)')
-
+        
 # Generate base mount point directories:
 createdir(base_path + '/ewf_' + args.dir_suffix)
 createdir(base_path + '/windows_' + args.dir_suffix)
 createdir(base_path + '/vss_' + args.dir_suffix)
+createdir(base_path + '/vsscarve_' + args.dir_suffix)
 createdir(base_path + '/shadow_' + args.dir_suffix)
+createdir(base_path + '/shadowcarve_' + args.dir_suffix)
 # Generate vss* subdirectories:
 for num in range(args.number):
     createdir(base_path + '/shadow_' + args.dir_suffix +'/vss' + str(num + 1))
+for num in range(args.number):
+    createdir(base_path + '/shadowcarve_' + args.dir_suffix +'/vss' + str(num + 1))
